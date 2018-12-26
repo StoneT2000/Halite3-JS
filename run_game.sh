6dc157989c36cac3916d1bb039b129cea6ab4964
StoneT2000 <stoneztao@gmail.com>
2018-12-26 14:56:59 +0800

    Update
    - Fixed bug where bots tag along to a builder but that builder didnt get designated
    - Ships are designated to build if they will probably have enough halite to immedietely build upon arrival
    - Made new bot files

--- run_game.sh
+++ run_game.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./halite --replay-directory replays/ -vvv --width 40 --height 40 -s 1545802078 "node ./bots_archive/currentBot/MyBot.js" "node ./bots_archive/currentBotTest/MyBot.js" "node ./bots_archive/currentBotTest/MyBot.js" "node ./bots_archive/currentBotTest/MyBot.js"
+./halite --replay-directory replays/ -vvv --width 64 -s 1545805219 "node ./bots_archive/currentBot/MyBot.js" "node ./bots_archive/currentBotTest/MyBot.js"
