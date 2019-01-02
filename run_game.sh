718940a865445bba0874d4bd94c511bbe06e9532
StoneT2000 <stoneztao@gmail.com>
2019-01-02 10:59:39 +0800

    Update
    - In 4 player games, bot never attacks

--- run_game.sh
+++ run_game.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./halite --replay-directory replays/ -vvv --width 32 --height 32 -s 1546389926 "node ./bots_archive/currentBot/MyBot.js" "node ./bots_archive/currentBotTest/MyBot.js"
+./halite --replay-directory replays/ -vvv --width 48 --height 48 -s 1546389329 "node ./bots_archive/currentBot/MyBot.js" "node ./bots_archive/currentBotTest/MyBot.js" "node ./bots_archive/currentBotTest/MyBot.js" "node ./bots_archive/currentBotTest/MyBot.js"
